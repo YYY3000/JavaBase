508356c0146ad99da98e00c93bbda5d5dfbe5dca
yyy3000 <yyy123456789>
2018-05-14 18:27:11 +0800

    [新增] 新增日志记录和数据库操作基类

---
+++ src/db/ResultSetHandler.java
@@ -0,0 +1,20 @@
+package db;
+
+/**
+ * @author yinyiyun
+ * @date 2018/5/2 10:32
+ */
+
+import java.sql.ResultSet;
+
+public interface ResultSetHandler {
+
+    /**
+     * 处理sql查询记录
+     *
+     * @param resultSet 查询结果
+     * @throws Exception
+     */
+    void handle(ResultSet resultSet) throws Exception;
+
+}
